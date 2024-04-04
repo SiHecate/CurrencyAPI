89a727e8ff72e636551bb9404095b91743dd01ef
SiHecate <blrumutcan64@gmail.com>
2024-04-04 12:13:19 +0300

    Dependcy injection and folder structure update

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"Currency/database"
+	"Currency/pkg"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/websocket/v2"
 	"github.com/joho/godotenv"
 )
 
@@ -14,21 +17,63 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	database.Database()
 
 	app := fiber.New()
 
+	// API endpointlerini tanımla
+	ApiRoutes(app)
+	WebsocketHandler(app)
+
+	// Middleware'ler
 	app.Use(logger.New())
 	app.Use(recover.New())
 
-	websocketHandler(app)
 	log.Fatal(app.Listen(":8080"))
 }
 
-func router(app *fiber.App) {
+// ApiRoutes fonksiyonu API endpointlerini tanımlar
+func ApiRoutes(app *fiber.App) {
+	tokenService := pkg.NewTokenService()
+	currencyService := pkg.NewCurrencyService()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PATCH, DELETE",
+		AllowCredentials: false,
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from Currency API! This is only for my personal use.")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return tokenService.Check(c)
+	})
+
+	app.Get("/token/create", tokenService.Generate)
+	app.Get("/token/list", tokenService.List)
+	app.Get("/currency", currencyService.CurrencyHandler)
 }
 
-// Websocket handler
-func websocketHandler(app *fiber.App) {
+// WebsocketHandler fonksiyonu websocket endpointini tanımlar
+func WebsocketHandler(app *fiber.App) {
+	currencyService := pkg.NewCurrencyService()
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	})
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		if c.Locals("allowed") == true {
+			if err := currencyService.Updater(c); err != nil {
+				c.Close()
+			}
+		}
+	}))
 }
